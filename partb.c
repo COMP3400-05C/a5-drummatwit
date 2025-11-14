14b5a7cc003bea9bef4cb3b16ed25f6acc22b055
drummatwit <drumm@wit.edu>
2025-11-14 13:27:57 -0500

    Was able to complete code and recive credit for the first test, was getting bats based errors for tests 2 and 3

--- partb.c
+++ partb.c
@@ -7,5 +7,36 @@
 
 int main(int argc, char* argv[]) {
     // TODO: Complete and document
-    return 0;
+    if (argc != 3) {
+        fprintf(stderr, "USAGE: partb FILEIN FILEOUT\n");
+        return 1;
+    }
+
+    char *input_filename = argv[1];
+    char *output_filename = argv[2];
+
+    char *parta_args[3];
+    parta_args[0] = "./parta";
+    parta_args[1] = input_filename;
+    parta_args[2] = NULL;
+
+    // Open output file
+    int fd = open(output_filename, O_WRONLY | O_CREAT | O_TRUNC, 0770);
+    if (fd < 0) {
+        perror("open");
+        return 1;
+    }
+
+    if (dup2(fd, STDOUT_FILENO) < 0) {
+        perror("dup2");
+        return 1;
+    }
+    close(fd);
+    printf("Category,Count\n");
+    fflush(stdout);
+    int eret = execv("./parta", parta_args);
+    perror("execv");
+
+    return 1;
 }
+
